6682db99fb0c15011998ee3042e3bd440fd910aa
Rocio <rosa.muruchilaura@gmail.com>
2019-03-20 11:48:35 -0300

    Add files via upload

---
+++ Muruchi.Rosa/Clase_02/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clase_02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Sello.mensaje = "Hola";
+            Console.WriteLine(Sello.Imprimir());
+            Sello.color = ConsoleColor.Green;
+            Sello.ImprimirEnColor();
+            Sello.Borrar();
+            Console.ReadKey();
+
+            Sello.mensaje = "Este es un mensaje Celeste ";
+            Sello.color = ConsoleColor.Cyan;
+            Sello.ImprimirEnColor();
+            Console.ReadKey();
+
+            Sello.mensaje = "Vuelvo al color original";
+            Console.WriteLine(Sello.Imprimir());
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Muruchi.Rosa/Clase_02/Sello.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clase_02
+{
+    class Sello
+    {
+        //atributos
+        public static string mensaje;
+
+        public static ConsoleColor color;
+
+        //Metodos
+        public static string Imprimir()
+        {
+            return Sello.ArmarFormatoMensaje();
+        }
+        public static void Borrar()
+        {
+            Sello.mensaje = "";
+        }
+        public static void ImprimirEnColor()
+        {
+
+            Console.ForegroundColor = Sello.color;
+            Console.WriteLine( Sello.Imprimir());
+            Console.ForegroundColor = ConsoleColor.Gray;
+        }
+        private static string ArmarFormatoMensaje()
+        {
+            string mensajeConFormato = "";
+            int largoMensaje;
+            //string saltoLinea = "\n";
+            
+            if(Sello.mensaje.Length>0)
+            {
+                largoMensaje = Sello.mensaje.Length + 2;
+            }
+            else
+            {
+                largoMensaje = 0;
+            }
+            for(int i=0; i<largoMensaje; i++)
+            {
+                mensajeConFormato += "*";
+            }
+            mensajeConFormato += "\n*" + Sello.mensaje + "*\n";
+            for (int i = 0; i < largoMensaje; i++)
+            {
+                mensajeConFormato += "*";
+            }
+
+            return mensajeConFormato;
+        }
+    }
+}
