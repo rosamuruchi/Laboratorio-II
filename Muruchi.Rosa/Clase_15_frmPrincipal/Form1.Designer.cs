a12cc48774d317a26689b9689fe3aaf13f0cdf3c
alumno <alumno>
2019-05-15 12:02:06 -0300

    archivos

---
+++ Muruchi.Rosa/Clase_15_frmPrincipal/Form1.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace Clase_15_frmPrincipal
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtValor = new System.Windows.Forms.TextBox();
+            this.btnEnviar = new System.Windows.Forms.Button();
+            this.btnTraer = new System.Windows.Forms.Button();
+            this.listVisor = new System.Windows.Forms.ListBox();
+            this.btnBuscar = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.SuspendLayout();
+            // 
+            // txtValor
+            // 
+            this.txtValor.Location = new System.Drawing.Point(41, 33);
+            this.txtValor.Name = "txtValor";
+            this.txtValor.Size = new System.Drawing.Size(156, 20);
+            this.txtValor.TabIndex = 0;
+            this.txtValor.TextChanged += new System.EventHandler(this.txtValor_TextChanged);
+            // 
+            // btnEnviar
+            // 
+            this.btnEnviar.Location = new System.Drawing.Point(252, 33);
+            this.btnEnviar.Name = "btnEnviar";
+            this.btnEnviar.Size = new System.Drawing.Size(114, 23);
+            this.btnEnviar.TabIndex = 1;
+            this.btnEnviar.Text = "&Enviar";
+            this.btnEnviar.UseVisualStyleBackColor = true;
+            this.btnEnviar.Click += new System.EventHandler(this.btnEnviar_Click);
+            // 
+            // btnTraer
+            // 
+            this.btnTraer.Location = new System.Drawing.Point(41, 247);
+            this.btnTraer.Name = "btnTraer";
+            this.btnTraer.Size = new System.Drawing.Size(94, 23);
+            this.btnTraer.TabIndex = 3;
+            this.btnTraer.Text = "&Traer";
+            this.btnTraer.UseVisualStyleBackColor = true;
+            this.btnTraer.Click += new System.EventHandler(this.btnTraer_Click);
+            // 
+            // listVisor
+            // 
+            this.listVisor.FormattingEnabled = true;
+            this.listVisor.Location = new System.Drawing.Point(41, 100);
+            this.listVisor.Name = "listVisor";
+            this.listVisor.Size = new System.Drawing.Size(371, 121);
+            this.listVisor.TabIndex = 4;
+            this.listVisor.SelectedIndexChanged += new System.EventHandler(this.listVisor_SelectedIndexChanged);
+            // 
+            // btnBuscar
+            // 
+            this.btnBuscar.Location = new System.Drawing.Point(252, 71);
+            this.btnBuscar.Name = "btnBuscar";
+            this.btnBuscar.Size = new System.Drawing.Size(114, 23);
+            this.btnBuscar.TabIndex = 5;
+            this.btnBuscar.Text = "&Buscar";
+            this.btnBuscar.UseVisualStyleBackColor = true;
+            this.btnBuscar.Click += new System.EventHandler(this.btnBuscar_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog1_FileOk);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(422, 308);
+            this.Controls.Add(this.btnBuscar);
+            this.Controls.Add(this.listVisor);
+            this.Controls.Add(this.btnTraer);
+            this.Controls.Add(this.btnEnviar);
+            this.Controls.Add(this.txtValor);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtValor;
+        private System.Windows.Forms.Button btnEnviar;
+        private System.Windows.Forms.Button btnTraer;
+        private System.Windows.Forms.ListBox listVisor;
+        private System.Windows.Forms.Button btnBuscar;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+    }
+}
+
---
+++ Muruchi.Rosa/Clase_15_frmPrincipal/Form1.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Clase_15_frmPrincipal
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            
+            
+        }
+
+        private void listVisor_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtValor_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void btnEnviar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string path = "C:\\Users\\alumno\\Desktop\\miArchivo.txt";
+                //string path = AppDomain.CurrentDomain.BaseDirectory + "miArchivo.txt";
+                StreamWriter sw = new StreamWriter(path);
+                string datos = this.txtValor.Text; 
+                sw.WriteLine(datos);
+                
+                sw.Close();
+
+            }
+            catch(Exception b)
+            {
+                this.listVisor.Items.Add ( b.Message);
+            }
+        }
+
+        private void btnTraer_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                //string path = AppDomain.CurrentDomain.BaseDirectory + "miArchivo.txt";     1ero
+                //string path = "C:\\Users\\alumno\\Desktop\\miArchivo.txt";                 2do
+                string path = Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + "\\miArchivo.txt"; //acceso al Escritorio de la pc
+                StreamReader sr = new StreamReader(path);
+                string lectura=sr.ReadToEnd();
+                DialogResult newResult = this.openFileDialog1.ShowDialog();
+
+                do
+                {        
+                    this.listVisor.Items.Add(lectura);
+                       lectura = sr.ReadLine();
+
+                } while (lectura != null);
+
+                 sr.Close();
+            }
+            catch (Exception b)
+            {
+                this.listVisor.Items.Add(b.Message);
+            }
+        }
+
+        private void btnBuscar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                OpenFileDialog Uno = new OpenFileDialog(); //abre los archivos que tenemos de texto
+                //Uno.ShowDialog(); //muestra los que tengo
+
+                DialogResult newResult = this.openFileDialog1.ShowDialog(); //Uno.ShowDialog();
+
+                if(newResult != DialogResult.Cancel)
+                {
+                    MessageBox.Show(Uno.FileName);
+                    //this.listVisor.Items.Add(Uno.FileName);
+                }
+
+                string path = "\\miArchivo.txt";
+
+
+            }
+            catch (Exception b)
+            {
+                this.listVisor.Items.Add(b.Message);
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.openFileDialog1.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+
+            this.openFileDialog1.Title= "Seleccione un titulo: ";
+            this.openFileDialog1.Multiselect = true;
+            this.openFileDialog1.DefaultExt = "txt";
+            this.openFileDialog1.AddExtension = true;
+            this.openFileDialog1.Filter = "Archivo (*.txt) | *.txt" ;
+            
+            
+        }
+
+        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+            
+        }
+    }
+}
